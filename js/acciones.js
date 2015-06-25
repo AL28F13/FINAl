2facc629bb3a3f26eca7640d2c524b977b08e0cb
AL28F13 <fernandasevilla16@gmail.com>
2015-06-25 11:50:44 -0500

    seis

--- js/acciones.js
+++ js/acciones.js
@@ -1,19 +1,21 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
+//document.addEventListener("deviceready",function(){
 	
-	$('#btnmenu').on ('tap',function() {
-		navigator.vibrate(2000);
-		alert ("hola")
+	$('#btnmenu').on('tap',function() {
+		navigator.vibrate(1000);
+		alert ("fghjnk")
+	});
 	
 	$('.ui-btn-icon-bottom').on('tap', function (){
 		flash ($(this));
 		audio.play($(this).attr('id'));
+		
 	});
 	audio=window.plugins.LowLatencyAudio;
-	audio.preloadFX('btnmenu', 'audio/C.mp3', function(){}, function(msg){alert ("Error " + msg);});
+	audio.preloadFX('botones','audio/C.mp3',function(){},function(msg){alert ("Error " + msg)
+	
+	;});
 	
 }); 
-});
-
-});
+//});
